1879b8d0f4cbb060bc73bcd66ee8e15d6533a30f
Jack Rutherford <jackcrutherford41@gmail.com>
2025-06-04 23:55:25 -0400

    Starting to add alembic; wip. Not connecting to the schema correctly.

--- server/src/server/models/__init__.py
+++ server/src/server/models/__init__.py
@@ -1,2 +1,9 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+metadata = MetaData(schema=os.getenv("SCHEMA"))
 Base = declarative_base()
