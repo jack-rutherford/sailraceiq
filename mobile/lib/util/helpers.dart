a47f507dd9e8b939f93cda7b9ed6d5aa76199528
Jack Rutherford <jackcrutherford41@gmail.com>
2025-10-28 10:44:05 -0400

    Moved getCurrentSeason to helper function file

--- mobile/lib/util/helpers.dart
+++ mobile/lib/util/helpers.dart
@@ -7,6 +7,15 @@ void navIfDiff(BuildContext context, String routeName) {
   }
 }
 
+// Get current season based on month
+  String getCurrentSeason() {
+    final month = DateTime.now().month;
+    if (month >= 3 && month <= 5) return 'Spring';
+    if (month >= 6 && month <= 8) return 'Summer';
+    if (month >= 9 && month <= 11) return 'Fall';
+    return 'Winter';
+  }
+
 class ThemeNotifier extends ChangeNotifier {
   ThemeMode _themeMode = ThemeMode.system;
   ThemeMode get themeMode => _themeMode;
